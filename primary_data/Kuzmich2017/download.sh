e227c299e85a84a0bd575c13bf744df2f7ab3a62
Christopher Gaul <cgaul@kuliouou.local>
2022-07-20 17:32:22 +0200

    Script to download Kuzmich_2017_TrendsNFA data

---
+++ primary_data/Kuzmich2017/download.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# The following dataset contains structures of 80 non-fullerene acceptors from
+# the paper Kuzmich et al. (2017), DOI: 10.1039/C6EE03654F.
+# The geometries are optimzed at the DFT/B3LYP/6-31G* level.
+# Some properties (LUMO, LUMO+1) are provided in the paper
+# (Table 1 of Kuzmich_2017_TrendsNFA), use the "Acceptor's label" to join this
+# table with the zipped xyz files.
+# Further, reported PCEs of acceptor donor pairs are given in the table.
+# Download the supplementary material from the journal webpage:
+wget https://www.rsc.org/suppdata/c6/ee/c6ee03654f/c6ee03654f1.zip

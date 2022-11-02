93a2fa4c7d0646f0fdef96753dd073c5ab7e763c
Christopher Gaul <c.gaul@icamcyl.com>
2022-11-02 16:35:33 +0100

    Towards factoring out the multitask dataset

---
+++ scripts/multitask_data.py
@@ -0,0 +1,43 @@
+import os.path
+import schnetpack as spk
+from schnetpack.data.atoms import AtomsData, ConcatAtomsData
+from maltose.atoms import MultitaskAtomsData
+
+
+def join_multitask_data(data_base_dir):
+    train_sets, val_sets, test_sets = [], [], []
+    for dataset_name, mapping in [
+        ('qm9', [
+            ('HOMO-B3LYP', 'homo'), ('LUMO-B3LYP', 'lumo'), ('Gap-B3LYP', 'gap'), 
+            ('HOMO-PBE0', None), ('LUMO-PBE0', None), ('Gap-PBE0', None)]),
+        ('alchemy', [
+            ('HOMO-B3LYP', 'homo'), ('LUMO-B3LYP', 'lumo'), ('Gap-B3LYP', 'gap'), 
+            ('HOMO-PBE0', None), ('LUMO-PBE0', None), ('Gap-PBE0', None)]),
+        ('oe62', [
+            ('HOMO-B3LYP', None), ('LUMO-B3LYP', None), ('Gap-B3LYP', None), 
+            ('HOMO-PBE0', 'homo PBE0_vacuum'), ('LUMO-PBE0', 'lumo PBE0_vacuum'), ('Gap-PBE0', 'gap PBE0_vacuum')]),
+        ('hopv', [
+            ('HOMO-B3LYP', 'HOMO B3LYP/def2-SVP'), ('LUMO-B3LYP', 'LUMO B3LYP/def2-SVP'), ('Gap-B3LYP', 'Gap B3LYP/def2-SVP'),
+            ('HOMO-PBE0', 'HOMO PBE0/def2-SVP'), ('LUMO-PBE0', 'LUMO PBE0/def2-SVP'), ('Gap-PBE0', 'Gap PBE0/def2-SVP')])
+        ]:
+        dataset_file = os.path.join(data_base_dir, dataset_name, 'data.db')
+        if not os.path.exists(dataset_file):
+            print(dataset_file , 'does not exist!')
+            print('Please run the respective scipt in scripts/primary_data/ first,')
+            print('and make sure the --data-base-dir is specified correctly.')
+        dataset = AtomsData(dataset_file)
+        split_file = os.path.join(data_base_dir, dataset_name, 'split_v2.npz')
+        if not os.path.exists(split_file):
+            print(split_file, 'does not exist!')
+            print('Please run scripts/primary_data/unified_split.py first!')
+        train, val, test = spk.data.partitioning.train_test_split(
+            data=dataset, split_file=split_file)
+        train_sets.append(MultitaskAtomsData(train, mapping))
+        val_sets.append(MultitaskAtomsData(val, mapping))
+        test_sets.append(MultitaskAtomsData(test, mapping))
+    
+    train = ConcatAtomsData(train_sets)
+    val = ConcatAtomsData(val_sets)
+    test = ConcatAtomsData(test_sets)
+
+    return train, val, test
--- scripts/multitask_model_v01.py
+++ scripts/multitask_model_v01.py
@@ -13,14 +13,14 @@ from torch.optim import Adam
 import os
 import schnetpack as spk
 import schnetpack.atomistic.model
-from schnetpack.data.atoms import AtomsData, ConcatAtomsData
 from schnetpack.train import Trainer, CSVHook, ReduceLROnPlateauHook
 from schnetpack.train.metrics import MeanAbsoluteError
 
 from maltose.output_modules import Set2Set
-from maltose.atoms import MultitaskAtomsData
 from maltose.loss import build_gated_mse_loss
 from maltose.metrics import MultitaskMetricWrapper
+
+import multitask_data
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -54,40 +54,7 @@ batch_size = 16
 
 
 logging.info("Preparing the datasets")
-train_sets, val_sets, test_sets = [], [], []
-for dataset_name, mapping in [
-    ('qm9', [
-        ('HOMO-B3LYP', 'homo'), ('LUMO-B3LYP', 'lumo'), ('Gap-B3LYP', 'gap'), 
-        ('HOMO-PBE0', None), ('LUMO-PBE0', None), ('Gap-PBE0', None)]),
-    ('alchemy', [
-        ('HOMO-B3LYP', 'homo'), ('LUMO-B3LYP', 'lumo'), ('Gap-B3LYP', 'gap'), 
-        ('HOMO-PBE0', None), ('LUMO-PBE0', None), ('Gap-PBE0', None)]),
-    ('oe62', [
-        ('HOMO-B3LYP', None), ('LUMO-B3LYP', None), ('Gap-B3LYP', None), 
-        ('HOMO-PBE0', 'homo PBE0_vacuum'), ('LUMO-PBE0', 'lumo PBE0_vacuum'), ('Gap-PBE0', 'gap PBE0_vacuum')]),
-    ('hopv', [
-        ('HOMO-B3LYP', 'HOMO B3LYP/def2-SVP'), ('LUMO-B3LYP', 'LUMO B3LYP/def2-SVP'), ('Gap-B3LYP', 'Gap B3LYP/def2-SVP'),
-        ('HOMO-PBE0', 'HOMO PBE0/def2-SVP'), ('LUMO-PBE0', 'LUMO PBE0/def2-SVP'), ('Gap-PBE0', 'Gap PBE0/def2-SVP')])
-    ]:
-    dataset_file = os.path.join(data_base_dir, dataset_name, 'data.db')
-    if not os.path.exists(dataset_file):
-        print(dataset_file , 'does not exist!')
-        print('Please run the respective scipt in scripts/primary_data/ first,')
-        print('and make sure the --data-base-dir is specified correctly.')
-    dataset = AtomsData(dataset_file)
-    split_file = os.path.join(data_base_dir, dataset_name, 'split_v2.npz')
-    if not os.path.exists(split_file):
-        print(split_file, 'does not exist!')
-        print('Please run scripts/primary_data/unified_split.py first!')
-    train, val, test = spk.data.partitioning.train_test_split(
-        data=dataset, split_file=split_file)
-    train_sets.append(MultitaskAtomsData(train, mapping))
-    val_sets.append(MultitaskAtomsData(val, mapping))
-    test_sets.append(MultitaskAtomsData(test, mapping))
-
-train = ConcatAtomsData(train_sets)
-val = ConcatAtomsData(val_sets)
-test = ConcatAtomsData(test_sets)
+train, val, _ = multitask_data.join_multitask_data(data_base_dir)
 
 train_loader = spk.AtomsLoader(train, batch_size=batch_size, shuffle=True)
 val_loader = spk.AtomsLoader(val, batch_size=batch_size)

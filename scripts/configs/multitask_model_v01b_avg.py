0f04f9f8aa3d1d72d824b0280c437e4c2cb67815
Christopher Gaul <c.gaul@icamcyl.com>
2022-11-16 16:24:17 +0100

    Variant of multitask_model_v01_avg with smaller batch size

---
+++ scripts/configs/multitask_model_v01b_avg.py
@@ -0,0 +1,61 @@
+"""
+Multitask Model trained on the Unified DB
+- The four datasets QM9, Alchemy, OE62, and HOPV, with the unified split
+  (split v2) are used.
+- Two sets of molecular energies are trained: (HOMO, LUMO, gap) x (B3LYP, PBE0)
+- With respect to multiask_model ("" a.k.a. "_v01"), the Set2Set output module
+  is replaced with the avg output module.
+- Batch size reduced to 8.
+"""
+import torch
+from torch.optim import Adam
+import schnetpack as spk
+import schnetpack.atomistic.model
+from schnetpack.train import ReduceLROnPlateauHook, CSVHook
+from schnetpack.train.metrics import MeanAbsoluteError
+
+from maltose.loss import build_gated_mse_loss
+from maltose.metrics import MultitaskMetricWrapper
+
+
+properties = ['HOMO-B3LYP', 'LUMO-B3LYP', 'Gap-B3LYP', 'HOMO-PBE0', 'LUMO-PBE0', 'Gap-PBE0']
+datasets = ['qm9', 'alchemy', 'oe62', 'hopv']
+batch_size = 8
+
+# For reproducibility, use precomputed statistics for mean and std
+divide_by_atoms = False
+meanstensor = torch.tensor([-6.34, 0.18, 6.52, -6.49, -1.58, 4.90])
+stddevstensor = torch.tensor([0.68, 1.29, 1.52, 0.72, 0.90, 1.18])
+
+# model build
+def build_model():
+    means = {k: v for k, v in zip(properties, meanstensor)}
+    stddevs = {k: v for k, v in zip(properties, stddevstensor)}
+    representation = spk.SchNet(n_interactions=6)
+    output_modules = [
+        spk.atomistic.Atomwise(
+            n_in=representation.n_atom_basis,
+            n_layers=2,
+            n_out=1,
+            mean=means[prop],
+            stddev=stddevs[prop],
+            aggregation_mode="avg",
+            property=prop,
+        ) for prop in properties]
+    return schnetpack.AtomisticModel(representation, output_modules)
+
+def build_optimizer(model):
+    return Adam(model.parameters(), lr=1e-4)
+
+def build_hooks(optimizer, log_path):
+    metrics = [MultitaskMetricWrapper(MeanAbsoluteError(p, p)) for p in properties]
+    return [
+        CSVHook(log_path=log_path, metrics=metrics),
+        ReduceLROnPlateauHook(
+            optimizer,
+            min_lr=0.5e-6,
+            stop_after_min=True),
+    ]
+
+def build_loss():
+    return build_gated_mse_loss(properties)
